7e85fd7cb6d87e8bd489b3bacfb9266b4c333b68
David Gilady <david.g@qualisystems.com>
2019-12-12 22:22:24 +0200

    fixed variable name

--- debug.ps1
+++ debug.ps1
@@ -3,6 +3,6 @@
 $scriptName = 'initial_tc_agent_setup.ps1'
 $content = ((New-Object System.Net.WebClient).DownloadString("https://raw.githubusercontent.com/QualiSystems/devops-scripts/master/$scriptName"))
 $initialScriptPath = Join-Path -Path $Env:TEMP -ChildPath $scriptName
-New-Item -Path $initialScriptPath -Value $c
+New-Item -Path $initialScriptPath -Value $content
 
 . $initialScriptPath -DebugMode
