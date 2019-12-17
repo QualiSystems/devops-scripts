f5ab8528e22666725198e9a536b4767531c87f97
David Gilady <david.g@qualisystems.com>
2019-12-17 16:14:25 +0200

    fixing script name

--- debug.ps1
+++ debug.ps1
@@ -1,6 +1,6 @@
 #Set-ExecutionPolicy Bypass -Scope Process -Force; iex ((New-Object System.Net.WebClient).DownloadString('https://raw.githubusercontent.com/QualiSystems/devops-scripts/master/debug.ps1'))
 
-$scriptName = 'initial_tc_agent_setup.ps1'
+$scriptName = 'agent.ps1'
 $content = ((New-Object System.Net.WebClient).DownloadString("https://raw.githubusercontent.com/QualiSystems/devops-scripts/master/$scriptName"))
 $initialScriptPath = Join-Path -Path $Env:TEMP -ChildPath $scriptName
 New-Item -Path $initialScriptPath -Value $content
