6f6a3126081859bee703e7b6152746e8568f992f
David Gilady <david.g@qualisystems.com>
2021-03-18 17:00:53 +0200

    fix multiline string assignment

--- setup_tc_agent.ps1
+++ setup_tc_agent.ps1
@@ -134,16 +134,16 @@ try {
     # }
 
     Log 'Configuring SQL Server maximum server memory'
-    $query =
-    "sp_configure 'show advanced options', 1;
-    GO
-    RECONFIGURE;
-    GO
-    sp_configure 'max server memory', 4096;
-    GO
-    RECONFIGURE;
-    GO
-    "
+    $query = @"
+sp_configure 'show advanced options', 1;
+GO
+RECONFIGURE;
+GO
+sp_configure 'max server memory', 4096;
+GO
+RECONFIGURE;
+GO
+"@
 
     Invoke-Sqlcmd -ServerInstance '.' -Query $query
 
